91470b59d5fbfc63d8c130fad60be2f1e5893248
benjamin <benjaminschug@gmail.com>
2017-09-08 00:14:14 +0200

    Better routing of nginx/flask
    Changed api root path from /filter/api/ to /api/filter/.
    Nginx now matches on that prefix and no longer sends every
    not-found file url to flask.
    This allows us to later add other apps (poe.gg) easily.

--- src/api/api.py
+++ src/api/api.py
@@ -1,7 +1,9 @@
+import os
 import json
 
 from box import Box
-from flask import Flask, request, render_template
+from flask import Flask, session, request, g, render_template, send_file
+import psycopg2
 
 import lootfilter
 
@@ -11,18 +13,51 @@ app.add_template_filter(lootfilter.templating.format_list_filter, 'names')
 app.add_template_filter(lootfilter.templating.setstyle_filter, 'setstyle')
 
 
-@app.route('/filter/api/build', methods=['POST'])
-def build_filter():
+def get_db():
+    """Open database connection if it's not already open."""
+    if not hasattr(g, 'postgres_db'):
+        g.postgres_db = psycopg2.connect(os.environ['FILTERCLOUD_DB'])
+    return g.postgres_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Close the database at the end of the request."""
+    if hasattr(g, 'postgres_db'):
+        g.postgres_db.close()
+
+
+@app.route('/api/filter/ping', methods=['GET'])
+def ping():
+    return "pong"
+
+
+@app.route('/api/filter/build', methods=['POST'])
+def build():
     try:
-        style = lootfilter.load_style(get_settings('style'))
-        config = lootfilter.load_config(get_settings('config'), style)
+        style = lootfilter.load_style(settings_from_post('style'))
+        config = lootfilter.load_config(settings_from_post('config'), style)
         return render_template('main.template', **config)
     except Exception as ex:
         app.logger.exception("/filter/api/build failed")
         return str(ex)
 
 
-def get_settings(key):
+@app.route('/api/filter/style/<id>', methods=['GET'])
+def style_instance(id):
+    filename = os.path.join(app.template_folder, 'style.json')
+    app.logger.info('Trying to return file %s' % filename)
+    return send_file(filename)
+
+
+@app.route('/api/filter/config/<id>', methods=['GET'])
+def config_instance(id):
+    if id == 'endgame' or id == 'leveling':
+        filename = os.path.join(app.template_folder, id + '.json')
+        return send_file(filename)
+
+
+def settings_from_post(key):
     text = request.form[key]
     obj = json.loads(text)
     return Box(obj)
