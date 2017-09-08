ef24dc56fa1742745b7eb0903ae0cea5a5a3a4e4
benjamin <benjaminschug@gmail.com>
2017-09-08 02:25:23 +0200

    Fix image paths, Add vue.js

--- static/filter/js/main.js
+++ static/filter/js/main.js
@@ -1,6 +1,20 @@
 (function( FilterCloud ) {
-    this.style = Style.load();
-    this.config = Config.load();
+    FilterCloud.app = null;
 
+    FilterCloud.init = function() {
+        console.log("Loading files before init...");
+        Promise.all([Style.load(), Config.load() ])
+        .then(function() {
+            console.log("Initializing now...")
+            FilterCloud.app = new Vue({
+                el: '#page',
+                data: {
+                    style: Style.data,
+                    config: Config.data,
+                    currentPage: null
+                }
+            });
+        });
+    };
 
 }( window.FilterCloud = window.FilterCloud || {} ));
