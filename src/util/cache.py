84a5400c6909f98a23621192adbf7e75ffd747eb
Benjamin Schug <benjamin.schug@gmail.com>
2017-09-12 09:35:50 +0000

    Add gc and exception handling to cache

--- src/util/cache.py
+++ src/util/cache.py
@@ -17,8 +17,15 @@ class Cache(object):
 
     Caveats:
      * This cache uses the function's __qualname__ to build a key.
+       If this is not unique for your function (e.g. same function name  defined in multiple modules),
+       functions with colliding names may return the other function's cached values.
+       Override the _function_key method to provide a different way of identifying functions,
+       or call get_with_key to directly specify a key (which must include the arguments).
+     * This cache is designed for functions with a finite set of possible arguments. It does not impose
+       any limits on the size of the cache. Don't use this if arguments are provided by user input or
+       some other source out of your control. A malicious user may make you run out of disk space.
     """
-    def __init__(self, name, *, dbname='cache.sqlite', timeout=None):
+    def __init__(self, name, *, dbname='cache.sqlite', timeout=None, use_outdated_cache_on_error=False, gc_on_init=True):
         """
         Multiple Cache instances can share the same database.
         They can also share the same table, i.e. they are different views on the same cache.
@@ -27,26 +34,18 @@ class Cache(object):
         :param name:    Name of the cache. Used as the SQLite table name.
         :param dbname:  SQLite db to use for storage.
         :param timeout: Maximum age of cached entries (timedelta)
+        :param gc_on_init: Garbage-collect outdated entries when initializing the Cache instance.
+        :param use_outdated_cache_on_error: If function call raises an exception, and there is an outdated value
+                                            still in the cache, return that instead of raising the exception.
         """
         assert timeout is not None
         self.name = name
         self.dbname = dbname
         self.timeout = timeout
+        self.use_outdated_cache_on_error = use_outdated_cache_on_error
         self._init_db()
-
-    @contextmanager
-    def _db_cursor(self):
-        with closing(sqlite3.connect(self.dbname, detect_types=sqlite3.PARSE_DECLTYPES, isolation_level=None)) as db:
-            with closing(db.cursor()) as cursor:
-                yield cursor
-                #db.commit()
-
-    def _init_db(self):
-        with self._db_cursor() as cursor:
-            cursor.execute('CREATE TABLE IF NOT EXISTS ' + self.name +
-                           ' (key TEXT PRIMARY KEY NOT NULL, ' +
-                           ' timestamp TIMESTAMP NOT NULL, ' +
-                           ' content BLOB NOT NULL)')
+        if gc_on_init:
+            self.gc()
 
     def get(self, function, *args, **kwargs):
         """
@@ -76,17 +75,25 @@ class Cache(object):
             return cached
         logger.debug("No cached version found for %s", format(key))
 
-        # Invoke function and store result in cache
-        new_value = function(*args, **kwargs)
-        self.store(key, new_value)
-        return new_value
+        try:
+            # Invoke function and store result in cache
+            new_value = function(*args, **kwargs)
+            self.store(key, new_value)
+            return new_value
+        except:
+            # Function call failed, use cached value even if outdated
+            cached = self.lookup_cached(key, threshold=datetime(1970, 1, 1))
+            if cached is not None and self.use_outdated_cache_on_error:
+                logger.debug("%s call failed, returning outdated value from cache", key)
+                return cached
+            raise
 
-    def lookup_cached(self, key):
+    def lookup_cached(self, key, *, threshold=None):
         """
         Lookup a value in the cache by key.
         If the value has never been added, or is outdated, returns None.
         """
-        threshold = datetime.utcnow() - self.timeout
+        threshold = threshold or datetime.utcnow() - self.timeout
 
         with self._db_cursor() as cursor:
             cursor.execute('SELECT timestamp, content FROM ' + self.name + ' WHERE key = ?', (key,))
@@ -106,3 +113,31 @@ class Cache(object):
             cursor.execute('INSERT OR REPLACE INTO ' + self.name + '(key, timestamp, content) ' +
                            'VALUES (?, CURRENT_TIMESTAMP, ?) ',
                            (key, value))
+
+    def gc(self):
+        """
+        Delete outdated entries from the database.
+        """
+        with self._db_cursor() as cursor:
+            threshold = datetime.utcnow() - self.timeout
+            cursor.execute('DELETE FROM ' + self.name + ' WHERE timestamp < ?', (threshold,))
+
+    @contextmanager
+    def _db_cursor(self):
+        # Connect with isolation_level=None for autocommit mode and close connection after each access to avoid 
+        # locking the database. Otherwise, you can't access the same database from multiple cache instances.
+        with closing(sqlite3.connect(self.dbname, detect_types=sqlite3.PARSE_DECLTYPES, isolation_level=None)) as db:
+            with closing(db.cursor()) as cursor:
+                yield cursor
+
+    def _init_db(self):
+        with self._db_cursor() as cursor:
+            cursor.execute('CREATE TABLE IF NOT EXISTS ' + self.name +
+                           ' (key TEXT PRIMARY KEY NOT NULL, ' +
+                           ' timestamp TIMESTAMP NOT NULL, ' +
+                           ' content BLOB NOT NULL)')
+
+    def _function_key(self, function):
+        """Override this if __qualname__ isn't unique for the functions you're using."""
+        return function.__qualname__
+
