c3f6161ca1efa698d4ad2c8e81157e69e0cbad09
benjamin <benjaminschug@gmail.com>
2017-12-12 00:49:30 +0100

    Actually fix slider-as-string bug

--- static/filter/js/components.js
+++ static/filter/js/components.js
@@ -316,7 +316,7 @@ Vue.component('linearslider', {
                 ga('send', 'event', 'configure', this.id);
             }
             this.lastUpdate = new Date();
-            this.$emit('input', 0+val);
+            this.$emit('input', parseInt(val));
         },
     },
 });
